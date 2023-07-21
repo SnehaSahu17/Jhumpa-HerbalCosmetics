522fc2463c127479ae1e1f475378c84fca57c459
Sneha Sahu <snehasahu@Snehas-MacBook-Air.local>
2023-07-22 00:19:41 +0530

    about us

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Products from './components/Products';
 import Footer from './components/Footer';
+import Aboutus from './components/Aboutus';
 
 function App() {
   return (
     <div className="w-full h-full webColor relative">
       <Navbar />
       <Home />
+      <Aboutus/>
       {/* <Products/> */}
       <div className="bg-[#173D28]">
         <Footer />
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Banner() {
   return (
-    <div className="grid grid-col-1 md:grid-cols-3 gap-32 pt-10 md:pb-40">
+    <div className="grid grid-col-1 md:grid-cols-3 gap-32 pt-10 md:pb-40 md:pt-0">
       <div className=" w-full h-fit flex flex-col justify-center items-center">
         <img src="https://res.cloudinary.com/dkqxnquga/image/upload/v1689921949/Isolation_Mode_egzvad.png"></img>
         <h1 className="py-4 text-[#318458] text-center text-5xl font-bold leading-[50px] tracking-[-0.5px]">
